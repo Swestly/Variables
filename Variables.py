9f101a2a05c4fe756fd1234c08a3e43197aabfa6
Swestly <brat02rus@gmail.com>
2024-11-17 10:53:53 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,8 @@
+homework = 12
+time = 1.5
+Course_name = 'Python'
+
+# // Перемены и взаимодейстовать с ними
+
+print('Курс:', Course_name, 'всего задачи', homework, 'затрачен часов:', time, 'среднее время выполнения', time / homework, 'часа.' )
+
